e26b09005d0bc45c0162bb9cf8d9132e64aaafa7
mathias8dev <kalipemathias.pro@gmail.com>
2024-02-08 18:13:36 +0100

    refactor: Moving to MQTTClientEvent to a separate file

---
+++ app/src/main/java/com/mathias8dev/mqttclient/domain/event/MQTTClientEvent.kt
@@ -0,0 +1,33 @@
+package com.mathias8dev.mqttclient.domain.event
+
+import com.mathias8dev.mqttclient.storage.room.model.Config
+
+abstract class MQTTClientEvent: Event() {
+
+    data object IDLE: MQTTClientEvent()
+    data object MQTTClientDisconnectedFromServer: MQTTClientEvent()
+    data object MQTTClientConnectedToServer: MQTTClientEvent()
+
+    abstract class MQTTClientException(
+        open val config: Config?,
+        open val cause: Throwable
+    ): MQTTClientEvent()
+    data class MQTTClientInitializationException(
+        override val config: Config?,
+        override val cause: Throwable
+    ): MQTTClientException(config, cause)
+
+    data class MQTTClientConnectException(
+        override val config: Config?,
+        override val cause: Throwable
+    ): MQTTClientException(config, cause)
+    data class MQTTClientTopicSubscriptionException(
+        override val config: Config?,
+        override val cause: Throwable
+    ): MQTTClientException(config, cause)
+
+    data class MQTTClientDisconnectException(
+        override val config: Config?,
+        override val cause: Throwable
+    ): MQTTClientException(config, cause)
+}
